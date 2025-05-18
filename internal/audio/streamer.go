add56ed28a613a9703f16d479b1bc773961caaab
Adam Elmaghraby <adamfingersen@gmail.com>
2025-05-17 20:56:30 -0500

    using dca package and got rid of gopus

--- internal/audio/streamer.go
+++ internal/audio/streamer.go
@@ -3,77 +3,40 @@ package audio
 import (
 	"context"
 	"fmt"
-	"io"
-	"os/exec"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
-	"layeh.com/gopus"
+	"github.com/jogramming/dca"
 )
 
 func StreamYouTube(ctx context.Context, url string, vc *discordgo.VoiceConnection) error {
+	
+	opts := dca.StdEncodeOptions
+	opts.RawOutput = true
+	opts.Bitrate = 96
+	opts.Application = "lowdelay"
 
-	ytdlp := exec.CommandContext(ctx, "yt-dlp", "-f", "bestaudio", "-o", "-", url)
-
-	ffmpeg := exec.CommandContext(ctx,
-        "ffmpeg",
-        "-i", "pipe:0",       
-        "-analyzeduration", "0",
-        "-loglevel", "0",
-        "-ac", "2",           
-        "-ar", "48000",       
-        "-f", "s16le",        
-        "pipe:1",             
-    )
-
-	r, w := io.Pipe()
-	ytdlp.Stdout = w
-	ffmpeg.Stdin = r
-
-	pcmOut, err := ffmpeg.StdoutPipe()
+	encodingSession, err := dca.EncodeFile(url, opts)
 	if err != nil {
-		return err
+		return fmt.Errorf("dca encode error: %w", err)
 	}
-
-	err = ytdlp.Start()
-	if err != nil {
-		return err
-	}
-
-	err = ffmpeg.Start()
-	if err != nil {
-		return err
-	}
+	defer encodingSession.Cleanup()
 
 	vc.Speaking(true)
 	defer vc.Speaking(false)
 
-	opusEncoder, err := gopus.NewEncoder(48000, 2, gopus.Audio)
-	if err != nil {
-    	return fmt.Errorf("could not create gopus encoder: %w", err)
-	}
+	done := make(chan error)
+	dca.NewStream(encodingSession, vc, done)
 
-	const frameSize = 1920
-	pcmBuf := make([]byte, frameSize)
-	opusBuf := make([]byte, 4000)
-
-	for {
-		n, err := io.ReadFull(pcmOut, pcmBuf)
-		if err != nil {
-			break
+	select{
+	case err := <-done:
+		if err != nil && err.Error() != "EOF" {
+			return fmt.Errorf("Stream error: %w", err)
 		}
-
-		frameSize, err := opusEncoder.Encode(pcmBuf[:n], opusBuf)
-			if err != nil {
-    			return fmt.Errorf("opus encode error: %w", err)
-			}
-
-		vc.OpusSend <- opusBuf[:frameSize]
-
+	case <-ctx.Done():
+		return fmt.Errorf("playback timed out after %v", 10*time.Minute)
 	}
 
-	w.Close()
-	ytdlp.Wait()
-	ffmpeg.Wait()
 
 	return nil
 }
