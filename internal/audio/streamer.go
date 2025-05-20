f4deeaabf3fd7c65eeb68f7f2db1a90bedfc5a70
Adam Elmaghraby <adamfingersen@gmail.com>
2025-05-19 21:15:36 -0500

    using youtube-dl to make temp audio file

--- internal/audio/streamer.go
+++ internal/audio/streamer.go
@@ -3,6 +3,8 @@ package audio
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/exec"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
@@ -10,13 +12,18 @@ import (
 )
 
 func StreamYouTube(ctx context.Context, url string, vc *discordgo.VoiceConnection) error {
-	
+	cmd := exec.Command("youtube-dl", "-x", "--audio-format", "mp3", "-o", "temp_audio.mp3", url)
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("youtube-dl error: %w", err)
+	}
+	defer os.Remove("temp_audio.mp3")
+
 	opts := dca.StdEncodeOptions
 	opts.RawOutput = true
 	opts.Bitrate = 96
 	opts.Application = "lowdelay"
 
-	encodingSession, err := dca.EncodeFile(url, opts)
+	encodingSession, err := dca.EncodeFile("temp_audio.mp3", opts)
 	if err != nil {
 		return fmt.Errorf("dca encode error: %w", err)
 	}
@@ -28,15 +35,14 @@ func StreamYouTube(ctx context.Context, url string, vc *discordgo.VoiceConnectio
 	done := make(chan error)
 	dca.NewStream(encodingSession, vc, done)
 
-	select{
+	select {
 	case err := <-done:
 		if err != nil && err.Error() != "EOF" {
-			return fmt.Errorf("Stream error: %w", err)
+			return fmt.Errorf("stream error: %w", err)
 		}
 	case <-ctx.Done():
 		return fmt.Errorf("playback timed out after %v", 10*time.Minute)
 	}
-
 
 	return nil
 }
