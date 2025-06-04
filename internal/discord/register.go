40645415fe4411267b33390bb24d7442ff5f35a9
Adam Elmaghraby <144628583+AdamElmaghraby@users.noreply.github.com>
2025-06-04 13:10:20 -0500

    fix: return errors from command registration

--- internal/discord/register.go
+++ internal/discord/register.go
@@ -6,25 +6,25 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-
-
 func RegisterCommand(dg *discordgo.Session) error {
-	_, err := dg.ApplicationCommandCreate(dg.State.User.ID, "",	&discordgo.ApplicationCommand{
-    Name:        "play",
-    Description: "Play audio from a YouTube link",
-    Options: []*discordgo.ApplicationCommandOption{
-        {
-            Type:        discordgo.ApplicationCommandOptionString,
-            Name:        "url",
-            Description: "YouTube video URL to play",
-            Required:    true,
-        },
-    },
-    })
+	_, err := dg.ApplicationCommandCreate(dg.State.User.ID, "", &discordgo.ApplicationCommand{
+		Name:        "play",
+		Description: "Play audio from a YouTube link",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "url",
+				Description: "YouTube video URL to play",
+				Required:    true,
+			},
+		},
+	})
 	if err != nil {
-        log.Fatalf("Cannot create slash cmd: %v", err)
-    }
+		// log error but allow caller to decide how to handle it
+		log.Printf("Cannot create slash cmd: %v", err)
+		return err
+	}
 
-	return err
+	return nil
 
 }
